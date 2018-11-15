6cee4204fc400942758f4a76ef4e1335ca235a28
Ioannis Anagnostides <ioannis.anagnostides@gmail.com>
2018-11-15 02:31:02 +0200

    Another commit, modified .sql and added a Servlet file

--- back-end/database/web_app.sql
+++ back-end/database/web_app.sql
@@ -1,3 +1,4 @@
+DROP DATABASE web_app;
 
 CREATE DATABASE IF NOT EXISTS web_app;
 
@@ -8,47 +9,108 @@ CREATE TABLE IF NOT EXISTS user (
   first_name VARCHAR(15) NOT NULL,
   last_name VARCHAR(20) NOT NULL,
   user_name VARCHAR(20) UNIQUE, /* Should be unique */
-  password VARCHAR(20) NOT NULL,
+  password VARCHAR(20) NOT NULL, /* At least 10 characters */
   email VARCHAR(30) NOT NULL UNIQUE,
   phone_number BIGINT UNIQUE
 );
-
-/* Passoword constraint, consists of at least 10 characters */
 
 CREATE TABLE IF NOT EXISTS product (
   id INT AUTO_INCREMENT PRIMARY KEY,
   name VARCHAR(20) NOT NULL UNIQUE,
   company VARCHAR(20) NOT NULL,
   category VARCHAR(20) NOT NULL,
-  sub_category VARCHAR(20) NOT NULL,
-  stars /* 0.5, 1 ... 5 */,
+  stars DECIMAL(2,1) NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS store (
 
-  id INT AUTO_INCREMENT PRIMARY_KEY,
-  name VARCHAR(20) NOT NULL,
+
+
+CREATE TABLE IF NOT EXISTS store (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(20) NOT NULL
   /* Address */
 );
 
 CREATE TABLE IF NOT EXISTS has_product (
-
+  id INT AUTO_INCREMENT PRIMARY KEY,
   FOREIGN KEY user_id(id)
   REFERENCES user(id),
   FOREIGN KEY product_id(id)
   REFERENCES product(id),
-  FOREIGN KEY store_id (id)
+  FOREIGN KEY store_id(id)
   REFERENCES store(id),
   price DOUBLE(5,2) NOT NULL,
-  date DATE NOT NULL
-  stars NOT NULL /*0.5 1 ...5 */
+  date DATE NOT NULL,
+  stars DECIMAL(2,1) NOT NULL
 );
 
+/* First name constraint, only alphabetic characters are allowed */
+
+DELIMITER $$
+
+CREATE PROCEDURE `check_first_name`(IN first_name VARCHAR(15))
+BEGIN
+    IF (NOT(first_name REGEXP '^[A-Za-z]+$')) THEN
+        SIGNAL SQLSTATE '45000'
+            SET MESSAGE_TEXT = 'Check constraint on first_name failed';
+    END IF;
+END$$
+
+DELIMITER ;
+
+-- Before insert
+DELIMITER $$
+CREATE TRIGGER `first_name_before_insert` BEFORE INSERT ON `user`
+FOR EACH ROW
+BEGIN
+    CALL check_first_name(new.first_name);
+END$$
+DELIMITER ;
+
+-- before update
+DELIMITER $$
+CREATE TRIGGER `first_name_before_update` BEFORE UPDATE ON `user`
+FOR EACH ROW
+BEGIN
+    CALL check_first_name(new.first_name);
+END$$
+DELIMITER ;
 
 
+/* Last name constraint, only alphabetic characters are allowed */
+
+DELIMITER $$
+
+CREATE PROCEDURE `check_last_name`(IN last_name VARCHAR(20))
+BEGIN
+    IF (NOT(last_name REGEXP '^[A-Za-z]+$')) THEN
+        SIGNAL SQLSTATE '45000'
+            SET MESSAGE_TEXT = 'Check constraint on last_name failed';
+    END IF;
+END$$
+
+DELIMITER ;
+
+-- Before insert
+DELIMITER $$
+CREATE TRIGGER `last_name_before_insert` BEFORE INSERT ON `user`
+FOR EACH ROW
+BEGIN
+    CALL check_last_name(new.last_name);
+END$$
+DELIMITER ;
+
+-- before update
+DELIMITER $$
+CREATE TRIGGER `last_name_before_update` BEFORE UPDATE ON `user`
+FOR EACH ROW
+BEGIN
+    CALL check_last_name(new.last_name);
+END$$
+DELIMITER ;
 
 
-
+/* Password constraint, at least 10 characters */
 
 DELIMITER $$
 
@@ -113,6 +175,40 @@ DELIMITER ;
 
 
 
+/* Stars constraint, values in {0.5, 1.0, 1.5, 2.0, 2.5, 3.0, 3.5, 4.0, 4.5, 5.0} */
+
+DELIMITER $$
+
+CREATE PROCEDURE `check_stars`(IN stars DECIMAL(2,1))
+BEGIN
+    IF (stars > 5 OR stars < 0.5 OR FLOOR(2*stars) <> 2*stars) THEN
+        SIGNAL SQLSTATE '45000'
+            SET MESSAGE_TEXT = 'Check constraint on stars failed';
+    END IF;
+END$$
+
+DELIMITER ;
+
+-- Before insert
+DELIMITER $$
+CREATE TRIGGER `stars_before_insert` BEFORE INSERT ON `product`
+FOR EACH ROW
+BEGIN
+    CALL check_stars(new.stars);
+END$$
+DELIMITER ;
+
+-- before update
+DELIMITER $$
+CREATE TRIGGER `stars_before_update` BEFORE UPDATE ON `product`
+FOR EACH ROW
+BEGIN
+    CALL check_stars(new.stars);
+END$$
+DELIMITER ;
+
+INSERT INTO product (name, company, category, stars)
+VALUES ('P8-Lite', 'Huawei', 'Mobile', 4.5);
 
 INSERT INTO user (first_name, last_name, user_name, password, email, phone_number)
 VALUES ('John', 'Doe', 'johnDoe', '1234567891', 'johnDoe@ntua.gr', 1234567891),
